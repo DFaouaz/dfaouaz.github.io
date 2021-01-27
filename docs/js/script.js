591a600520ed6d07286e47f6d99f03792d830225
DFaouaz <dfaouaz@ucm.es>
2021-01-27 23:19:04 +0100

    Style changed

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,17 +1,17 @@
-const burger = document.getElementById("burger");
-const burger_menu = document.getElementById("burger_menu");
+// const burger = document.getElementById("burger");
+// const burger_menu = document.getElementById("burger_menu");
 
-burger.addEventListener("click", function () {
-    if (burger.classList.contains("opened")) {
-        burger.classList.replace("opened", "closed");
-        burger_menu.classList.replace("opened", "closed");
-    }
-    else if (burger.classList.contains("closed")){
-        burger.classList.replace("closed", "opened");
-        burger_menu.classList.replace("closed", "opened");
-    }
-    else {
-        burger.classList.add("closed");
-        burger_menu.classList.add("closed");
-    }
-});
+// burger.addEventListener("click", function () {
+//     if (burger.classList.contains("opened")) {
+//         burger.classList.replace("opened", "closed");
+//         burger_menu.classList.replace("opened", "closed");
+//     }
+//     else if (burger.classList.contains("closed")){
+//         burger.classList.replace("closed", "opened");
+//         burger_menu.classList.replace("closed", "opened");
+//     }
+//     else {
+//         burger.classList.add("closed");
+//         burger_menu.classList.add("closed");
+//     }
+// });

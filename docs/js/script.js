793c9a6f974fdaf1671c17d7b3323780bf075c49
DFaouaz <dfaouaz@ucm.es>
2021-01-28 20:02:42 +0100

    Full view-port style

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,13 +5,16 @@ burger.addEventListener("click", function () {
     if (burger.classList.contains("opened")) {
         burger.classList.replace("opened", "closed");
         burger_menu.classList.replace("opened", "closed");
+        document.body.classList.remove("noscroll");
     }
     else if (burger.classList.contains("closed")){
         burger.classList.replace("closed", "opened");
         burger_menu.classList.replace("closed", "opened");
+        document.body.classList.add("noscroll");
     }
     else { // Si no encuentra nada, esta cerrado y se abré
         burger.classList.add("opened");
         burger_menu.classList.add("opened");
+        document.body.classList.add("noscroll");
     }
 });
